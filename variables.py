59bf7780647acf8925d3a88255784ea60a5b7504
MateBalint <matebalint98@gmail.com>
2018-08-23 11:12:17 +0200

    Variables.py

---
+++ variables.py
@@ -0,0 +1,5 @@
+tic_tac_toe_table = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+list_of_player_inputs = [] 
+possible_player_inputs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+possible_player_inputs_for_restart = ["y", "n"]  
+x_or_o_variable = "X"
